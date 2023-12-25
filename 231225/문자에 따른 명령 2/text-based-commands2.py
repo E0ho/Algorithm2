27cb482a6c5e3592a8d55502e45fde7e9306b683
E0ho <110147055+E0ho@users.noreply.github.com>
2023-12-25 18:35:27 +0900

    Title: 문자에 따른 명령 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231225/문자에 따른 명령 2/text-based-commands2.py
@@ -0,0 +1,20 @@
+x, y = 0, 0
+dir_num = 0
+
+s = input()
+
+# 북 동 남 서
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+
+for c in s:
+    if c == "L":
+        dir_num -= 1
+    elif c == "R":
+        dir_num += 1
+    elif c == "F":
+        x, y = x + dx[dir_num], y + dy[dir_num]
+    else:
+        print("잘못된 입력")
+
+print(x, y)
