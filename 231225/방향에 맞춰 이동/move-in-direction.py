d3c79cf6785b741b4d4fd28d48136540ad2fd476
E0ho <110147055+E0ho@users.noreply.github.com>
2023-12-25 18:26:16 +0900

    Title: 방향에 맞춰 이동, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 231225/방향에 맞춰 이동/move-in-direction.py
+++ 231225/방향에 맞춰 이동/move-in-direction.py
@@ -5,19 +5,13 @@ n = int(input())
 dx = [1, 0, -1, 0]
 dy = [0, -1, 0, 1]
 
+dir_map = {"E":0, "S":1, "W":2, "N":3}
      
 for i in range(n):
     dir_num, repeat = input().split()
     repeat = int(repeat)
 
-    if dir_num == "E":
-        dir_num = 0
-    elif dir_num == "S":
-        dir_num = 1
-    elif dir_num == "W":
-        dir_num = 2
-    else:
-        dir_num = 3
+    dir_num = dir_map[dir_num]
 
     for i in range(repeat):
         x, y = x + dx[dir_num], y + dy[dir_num]
