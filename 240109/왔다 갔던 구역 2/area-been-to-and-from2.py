63f0e8f7575347fd8458ed86a3138ae7f9154741
E0ho <110147055+E0ho@users.noreply.github.com>
2024-01-09 00:15:51 +0900

    Title: 왔다 갔던 구역 2, Time: 137ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240109/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -0,0 +1,27 @@
+n = int(input())
+
+x = 100
+
+count_list = [
+    0 for i in range(200)
+]
+
+for _ in range(n):
+    count, dir = input().split()
+    count = int(count)
+
+    for _ in range(count):
+        if dir == "R":
+            x += 1
+            count_list[x] += 1
+
+        else:
+            x -= 1
+            count_list[x] += 1
+
+cnt = 0
+for ele in count_list:
+    if ele >= 2:
+        cnt += 1
+
+print(cnt)
