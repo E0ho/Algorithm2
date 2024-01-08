f76fb304cf830a73f8dcd369e4476ecda3827c0d
E0ho <110147055+E0ho@users.noreply.github.com>
2024-01-08 16:09:02 +0900

    Title: 알파벳 출력하기, Time: 136ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240108/알파벳 출력하기/print-alphabet.py
+++ 240108/알파벳 출력하기/print-alphabet.py
@@ -4,11 +4,11 @@ o = ord('A')
 
 for i in range(n):
     for j in range(i+1):
-        print(chr(o), end="")
-        o += 1
 
         if chr(o) == "Z":
-            
             o = ord('A')
+
+        print(chr(o), end="")
+        o += 1
 
     print()
