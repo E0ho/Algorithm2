3e13674b389068ce6959b1624638d58bc98a2159
E0ho <110147055+E0ho@users.noreply.github.com>
2024-01-08 18:38:29 +0900

    Title: 3n + 1 수열 m번, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240108/3n + 1 수열 m번/3n-plus-1-sequence-m-times.py
@@ -0,0 +1,19 @@
+m = int(input())
+n = int(input())
+
+# n 짝수 = /2
+# n 홀수 = *3 +1
+
+# n이 1이 될때 까지
+
+for _ in range(m):
+    cnt = 0
+    while n != 1:
+        cnt += 1
+
+        if n % 2 == 0:
+            n = n / 2
+        else:
+            n = n * 3 + 1
+
+    print(cnt)
