76b87622e0c15fe42ac31ea3e2caef2ccebdbfe1
E0ho <110147055+E0ho@users.noreply.github.com>
2024-01-08 21:49:39 +0900

    Title: 연속부분수열일까, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240108/연속부분수열일까/contiguous-array-or-not.py
+++ 240108/연속부분수열일까/contiguous-array-or-not.py
@@ -5,13 +5,19 @@ b_list = list(map(int, input().split()))
 
 
 for i in range(n1):
-
+# 9 2 4 1 3 3 1 4 6 8
+#     4 1 3 3 1 4
     if a_list[i] == b_list[0]:
         flag = True
-        for j in range(i, i + n2 + 1):
-            if a_list[j] != b_list[j - i]:
+        for j in range(n2):
+            if a_list[i+j] != b_list[j]:
                 flag = False
                 break
+            else:
+                pass
+    
+    else:
+        flag = False
 
 if flag:
     print("Yes")
