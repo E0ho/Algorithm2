7e3f40aceb5a8b352b0d5de882b35ccdc3ec6b8b
E0ho <110147055+E0ho@users.noreply.github.com>
2024-01-08 15:04:01 +0900

    Title: 별표 출력하기 12, Time: 106ms, Memory: 24MB, Status: Passed - CodeTree

--- 240108/별표 출력하기 12/print-star-12.py
+++ 240108/별표 출력하기 12/print-star-12.py
@@ -1,23 +1,17 @@
 n = int(input())
 
-# r == 0 전체 출력
+# i = 0
+# c >= r
 
-# c가 홀수 and c >= r 이면, 출력
-
-
-for r in range(n):
-    
+for i in range(n):
     for j in range(n):
+        if i == 0:
+            print("*", end = " ")
 
-        # 0행 출력
-        if r == 0:
-            print("*", end=" ")
-
-        # 0행 이상 -> c > r and c 홀수
-        elif j % 2 != 0 and j >= r:
-            print("*", end=" ")
-
+        elif j >= i and j % 2 != 0:
+            print("*", end = " ")
+        
         else:
-            print(" ", end=" ")
+            print(" ", end = " ")
 
     print()
