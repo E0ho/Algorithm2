aa56dbf5b2261ef469c46103b22594786d564560
E0ho <110147055+E0ho@users.noreply.github.com>
2024-01-09 23:20:10 +0900

    Title: 대각선으로 숫자 채우기, Time: 249ms, Memory: 25MB, Status: Passed - CodeTree

--- 240109/대각선으로 숫자 채우기/diagonal-numbering.py
+++ 240109/대각선으로 숫자 채우기/diagonal-numbering.py
@@ -1,29 +1,26 @@
-n, m = map(int,input().split())
+n, m = map(int, input().split())
 
 grid = [
     [0] * m
     for _ in range(n)
 ]
 
+def in_range(r, c):
+    return (0 <= r and 0 <= c and r < n and c < m)
+
+dr = 1
+dc = -1
 cnt = 1
+
 for i in range(n):
     for j in range(m):
-        # 모든 배열 탐색 (0 이라면 cnt)
-        if grid[i][j] == 0:
-            grid[i][j] = cnt
+        temp_i = i
+        temp_j = j
+        while in_range(temp_i, temp_j) and grid[temp_i][temp_j] == 0:
+            grid[temp_i][temp_j] = cnt
             cnt += 1
-        
-        # 대각 이동
-        s_row = i
-        s_col = j
-
-        # col이 0 보다 클때, row가 n-1보다 작을때
-        while (s_col > 0) and (s_row < n - 1):
-            s_col -= 1
-            s_row += 1
-            if grid[s_row][s_col] == 0:
-                grid[s_row][s_col] = cnt
-                cnt += 1
+            temp_i = temp_i + dr
+            temp_j = temp_j + dc
 
 for i in range(n):
     for j in range(m):
