3ff565ba459627dfd4c319477c1176f3288c07d3
E0ho <110147055+E0ho@users.noreply.github.com>
2024-01-09 22:49:58 +0900

    Title: 문자에 따른 명령 2, Time: 134ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240109/문자에 따른 명령 2/text-based-commands2.py
+++ 240109/문자에 따른 명령 2/text-based-commands2.py
@@ -9,10 +9,10 @@ dys = [-1, 0, 1, 0]
 
 for c in command:
     if c == "L":
-        dir_num -= 1
+        dir_num = ((dir_num - 1) + 4) % 4
 
     elif c == "R":
-        dir_num += 1
+        dir_num = (dir_num + 1) % 4
 
     else:
         x = x + dxs[dir_num]
