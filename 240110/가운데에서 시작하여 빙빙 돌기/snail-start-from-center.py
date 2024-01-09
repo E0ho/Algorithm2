b0de9a80fc94712ab91a77af58cd8a468382435f
E0ho <110147055+E0ho@users.noreply.github.com>
2024-01-10 01:47:27 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 137ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240110/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
+++ 240110/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
@@ -11,6 +11,7 @@ r = n // 2
 c = n // 2
 dir_num = 0
 num = 1
+count = 1
 
 # 방향 (동, 북, 서, 남)
 drs = [0, -1, 0, 1]
@@ -20,22 +21,31 @@ dcs = [1, 0, -1, 0]
 def in_range(r, c):
     return 0 <= r and 0 <= c and r < n and c < n
 
-
-for _ in range(n * n):
-    grid[r][c] = num
-    num += 1
-
-    nr = r + drs[dir_num]
-    nc = c + dcs[dir_num]
-
-    if not in_range(nr, nc) or grid[nr][nc] != 0:
-        dir_num = (dir_num + 1) % 4
+# 북 -> 서
+# 남 -> 동 
+while num < (n * n):
+    
+    for _ in range(count):
+        grid[r][c] = num
+        num += 1
         nr = r + drs[dir_num]
         nc = c + dcs[dir_num]
 
-    r = nr
-    c = nc
+        if not in_range(nr, nc):
+            break
 
+        else:
+            r = nr
+            c = nc
+    
+    dir_num = (dir_num + 1) % 4
+    nr = r + drs[dir_num]
+    nc = c + dcs[dir_num]
+
+    if dir_num == 0 or dir_num == 2:
+        count += 1
+
+   
 for i in range(n):
     for j in range(n):
         print(grid[i][j], end=" ")
