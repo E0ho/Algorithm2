3d3a4007694b9ce9dc7b3762df06d576cbb239c5
E0ho <110147055+E0ho@users.noreply.github.com>
2024-01-10 16:05:09 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240110/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -0,0 +1,31 @@
+N, K, P, T = tuple(map(int, input().split()))
+
+time_line = []
+
+people = [
+    0 for i in range(N)
+]
+
+people[P-1] = 1
+
+# 입력
+for _ in range(T):
+    t_x_y = list(map(int, input().split()))
+    time_line.append(t_x_y)
+
+# time_line 정렬
+time_line.sort()
+
+for ele in time_line:
+    x = ele[1] - 1
+    y = ele[2] - 1
+
+    if (people[x] >= 1 and people[x] <= 2) or (people[y] >= 1 and people[y] <= 2):
+        people[x] += 1
+        people[y] += 1
+
+for ele in people:
+    if ele > 0:
+        print(1, end="")
+    else:
+        print(0, end="")
