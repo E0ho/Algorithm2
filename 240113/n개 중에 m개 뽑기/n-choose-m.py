2e56776c1edffd5ce1d8327dcf6cec31e461c0af
E0ho <110147055+E0ho@users.noreply.github.com>
2024-01-13 18:21:09 +0900

    Title: n개 중에 m개 뽑기, Time: 139ms, Memory: 25MB, Status: Passed - CodeTree

--- 240113/n개 중에 m개 뽑기/n-choose-m.py
+++ 240113/n개 중에 m개 뽑기/n-choose-m.py
@@ -1,22 +1,41 @@
 n, m = tuple(map(int, input().split()))
 arr = []
+visited = [ 0 for _ in range(n+1) ]
+
 
 def choose(floor, num):
-    # 출력 조건
-    if floor == m + 1:
+    if floor == m+1:
         for ele in arr:
             print(ele, end=" ")
         print()
         return
 
-    # 1 ~ n이하 숫자 넣기
-    for i in range(1, n + 1):
-        if i > num:
+    for i in range(1, n+1):
+        if i > num and visited[i] == 0:
             arr.append(i)
-            choose(floor + 1, i)
+            visited[i] = 1
+            choose(floor + 1 , i)
             arr.pop()
+            visited[i] = 0
 
-        else:
-            pass
-    
+
 choose(1, 0)
+# def choose(floor, num):
+#     # 출력 조건
+#     if floor == m + 1:
+#         for ele in arr:
+#             print(ele, end=" ")
+#         print()
+#         return
+
+#     # 1 ~ n이하 숫자 넣기
+#     for i in range(1, n + 1):
+#         if i > num:
+#             arr.append(i)
+#             choose(floor + 1, i)
+#             arr.pop()
+
+#         else:
+#             pass
+    
+# choose(1, 0)
