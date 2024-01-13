887bde52047fa352bbce9441da05c7090eee0f4c
E0ho <110147055+E0ho@users.noreply.github.com>
2024-01-13 17:51:18 +0900

    Title: n개 중에 m개 뽑기, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240113/n개 중에 m개 뽑기/n-choose-m.py
+++ 240113/n개 중에 m개 뽑기/n-choose-m.py
@@ -1,54 +1,22 @@
 n, m = tuple(map(int, input().split()))
-
-# 각 자리의 수
 arr = []
-total = []
 
-def choose(floor):
-
-    # M개 조합 완성
-    if floor == m+1:
-
-        # 중복 검사
-        for i in range(len(arr)):
-            for j in range(i+1, len(arr)):
-                if arr[j] == arr[i]:
-                    return
-        
-        # 출력
-        # print(arr)
-        total.append(list(arr))
-        return
-    
-    # 가능한 모든 조합
-    for i in range(1, n+1):
-        arr.append(i)
-        choose(floor+1)
-        arr.pop()
-a = []
-choose(1)
-# print(total)
-for ele in total:
-    ele.sort()
-    a.append(ele)
-
-length = len(a)
-num = 1
-
-for i in range(length):
-    for j in range(i+1, length):
-        if a[i] == a[j]:
-            a.pop(i)
-            a.insert(0, num)
-            num += 1
-
-for ele in total:
-    ele.sort()
-    a.append(ele)
-     
-# print(a)
-for i in range(length):
-    if type(a[i]) == list:
-        for ele in a[i]:
+def choose(floor, num):
+    # 출력 조건
+    if floor == n+1:
+        for ele in arr:
             print(ele, end=" ")
         print()
+
+    # 1 ~ n이하 숫자 넣기
+    for i in range(1, n+1):
+        if i > num:
+            arr.append(i)
+            choose(floor+1, i)
+            arr.pop()
+
+        else:
+            pass
+    
+
+choose(1, 0)
