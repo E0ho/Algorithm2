725d3840bb81722645c6fb18303a8d8616cb666b
E0ho <110147055+E0ho@users.noreply.github.com>
2024-04-17 15:31:43 +0900

    Title: 특정 조건에 맞게 k개 중에 1개를 n번 뽑기, Time: 297ms, Memory: 30MB, Status: Passed - CodeTree

--- 240417/특정 조건에 맞게 k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition-under-constraint.py
+++ 240417/특정 조건에 맞게 k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition-under-constraint.py
@@ -1,18 +1,31 @@
 # 1 ~ k 숫자 n번 고르기
 # 연속해서 같은 숫자 3번 이상 제외
 
-from itertools import product
+# 변수 선언 및 입력
+k, n = tuple(map(int, input().split()))
+selected_nums = []
 
-# 입력
-k, n = map(int, input().split())
 
-li = []
-for i in range(1, k+1):
-    li.append(i)
-
-combi = list(product(li, repeat = n))
-
-for comb in combi:
-    for i in range(len(comb)):
-        print(comb[i], end= " ")
+# 선택된 원소들을 출력해줍니다.
+def print_permutation():
+    for num in selected_nums:
+        print(num, end = " ")
     print()
+
+
+def find_duplicated_permutations(cnt):
+    # n개를 모두 뽑은 경우 답을 출력해줍니다.
+    if cnt == n:
+        print_permutation()
+        return
+    
+    for i in range(1, k + 1):
+        if cnt >= 2 and i == selected_nums[-1] and i == selected_nums[-2]:
+            continue
+        else:
+            selected_nums.append(i)
+            find_duplicated_permutations(cnt + 1)
+            selected_nums.pop()
+
+
+find_duplicated_permutations(0)
