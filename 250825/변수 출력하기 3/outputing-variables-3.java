5de52be18e608a1609934abc009cf6281f9764f8
E0ho <110147055+E0ho@users.noreply.github.com>
2025-08-25 21:45:27 +0900

    Title: 변수 출력하기 3, Time: 88ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250825/변수 출력하기 3/outputing-variables-3.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a, b;
+        a = 1;
+        b = 2;
+        char c = 'C';
+
+        System.out.println( a + "->" + b + "->" + c);
+    }
+}
